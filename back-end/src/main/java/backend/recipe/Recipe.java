80433dba0bee466389d447a2c9eb5fb58fd47e10
vedestig <larsmtv@gmail.com>
2023-09-11 14:31:49 +0200

    initialized application

---
+++ back-end/src/main/java/backend/recipe/Recipe.java
@@ -0,0 +1,53 @@
+package backend.recipe;
+
+public class Recipe {
+
+    String id;
+    String name;
+    String drink;
+    String origin;
+    String instruction;
+    String thumbnail;
+    String tags;
+    String youtube;
+    String ingredient1;
+    String ingredient2;
+    String ingredient3;
+    String ingredient4;
+    String ingredient5;
+    String ingredient6;
+    String ingredient7;
+    String ingredient8;
+    String ingredient9;
+    String ingredient10;
+    String ingredient11;
+    String ingredient12;
+    String ingredient13;
+    String ingredient14;
+    String strIngredient15;
+    String strIngredient16;
+    String strIngredient17;
+    String strIngredient18;
+    String strIngredient19;
+    String strIngredient20;
+    String strMeasure1;
+    String strMeasure2;
+    String strMeasure3;
+    String strMeasure4;
+    String strMeasure5;
+    String strMeasure6;
+    String strMeasure7;
+    String strMeasure8;
+    String strMeasure9;
+    String strMeasure10;
+    String strMeasure11;
+    String strMeasure12;
+    String strMeasure13;
+    String strMeasure14;
+    String strMeasure15;
+    String strMeasure16;
+    String strMeasure17;
+    String strMeasure18;
+    String strMeasure19;
+    String strMeasure20;
+}
